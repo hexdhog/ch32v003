19f18435e37deb6e2f6fc1ed699d8661c7a949cf
hexdhog <hexdhog@gmail.com>
2025-02-08 15:55:12 +0100

    finished asm implementation

--- asm/src/start.s
+++ asm/src/start.s
@@ -40,6 +40,8 @@ start:
 
 .section .text
 
+.equ ms_to_tick, 48000000/8000
+
 .equ rcc_base, 0x40021000
 .equ flash_r_base, 0x40022000
 .equ gpio_pd_base, 0x40011400
@@ -53,7 +55,6 @@ main:
         li a0, rcc_base # a0 -> RCC register base address
         li a1, flash_r_base # a1 -> flash register base address
         li a2, gpio_pd_base # a2 -> gpio port d register base address
-        li a3, systck_base # a3 -> system tick register base address
 
         # RCC CTLR = 0x01000001
         #     PLL_ON: enable PLL
@@ -85,25 +86,26 @@ main:
 
         # wait until PLL is ready
         li t1, 0x02000000 # PLL_RDY mask
-pll_rdy_wait:
+.L_pll_rdy_wait:
         # RCC CTLR
         lw t0, 0(a0)
         and t0, t0, t1
-        beq t0, zero, pll_rdy_wait
+        beq t0, zero, .L_pll_rdy_wait
 
         # RCC CFGR0 = (RCC CFGR0 & ~(RCC CFGR0 SW)) | RCC CFGR0 SW PLL
         lw t0, 4(a0) # t0 = RCC CFGR0
         and t0, t0, 0xfffffffc # ~(RCC CFGR0 SW) = ~(0x00000003) = 0xfffffffc
         or t0, t0, 0x00000002 # RCC CFGR0 SW PLL = 0x00000002
+        sw t0, 4(a0)
 
         # wait until PLL is used as SYSCLK
         li t1, 0x0000000c # RCC CFGR0 SWS mask
         li t2, 0x00000008 # RCC CFGR0 SW PLL
-pll_use_wait:
+.L_pll_use_wait:
         # RCC CFGR0
         lw t0, 4(a0)
         and t0, t0, t1
-        bne t0, t2, pll_use_wait
+        bne t0, t2, .L_pll_use_wait
 
         # setup GPIO pin for led
         # enable GPIO port D
@@ -121,10 +123,60 @@ pll_use_wait:
         or t0, t0, t1
         sw t0, 0(a2)
 
-        li t0, 1 << led_pin
-        sw t0, 20(a2)
+        li t2, 1 << led_pin # pin mask
+.L_loop:
+        sw t2, 20(a2)
+        li a0, 100*ms_to_tick
+        call delay_systick
 
-1:      j 1b
+        sw t2, 16(a2)
+        li a0, 1000*ms_to_tick
+        call delay_systick
+
+        j .L_loop
+
+delay_systick:
+# SYSTCK->SR &= ~((uint32_t) 1); // clear count value comparison flag
+# SYSTCK->CMP = n; // count end value
+# SYSTCK->CNT = 0; // count start value
+# SYSTCK->CTLR |= 1; // turn on system counter
+# while (!(SYSTCK->SR & 1));
+# SYSTCK->CTLR &= ~((uint32_t) 1); // turn off system counter
+
+        li a1, systck_base # a1 -> system tick register base address
+
+        # stop system counter
+        lw t0, 0(a1)
+        and t0, t0, 0xfffffffe # set last bit (STE) to 0
+        sw t0, 0(a1)
+
+        # clear count value comparison flag
+        li t0, 0xfffffffe # t0 = ~(1)
+        sw t0, 4(a1)
+
+        # set count start value
+        sw zero, 8(a1)
+        
+        # set count end value
+        sw a0, 16(a1)
+
+        # start system counter
+        lw t0, 0(a1)
+        or t0, t0, 0x00000001 # set last bit (STE) to 1
+        sw t0, 0(a1)
+
+        # wait until count system counter has reached target number
+.L_wait:
+        lw t0, 4(a1)
+        and t0, t0, 0x00000001
+        beq t0, zero, .L_wait
+
+        # stop system counter
+        lw t0, 0(a1)
+        and t0, t0, 0xfffffffe # set last bit (STE) to 0
+        sw t0, 0(a1)
+
+        ret
 
 .section .text.isr_handler
 
